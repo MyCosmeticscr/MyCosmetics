e5effdff9064a8f9adf4d2c1f666470ee87c7fab
MyCosmetics <jugofeliz99@gmail.com>
2026-08-22 12:06:05 -0600

    Add event listeners to brand buttons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const brandButtons = document.querySelectorAll(".brand-card");
 
+brandButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const brand = button.dataset.brand;
+
+        console.log("Marca seleccionada:", brand);
+
+    });
+
+});
